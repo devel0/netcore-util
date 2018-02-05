c23d6ae98b4f7922c6145fc030d3692654494336
Lorenzo Delana <oss.devel@searchathing.com>
2018-02-05 09:04:28 +0100

    initial import

---
+++ Util.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace SearchAThing.NETCoreUtil
+{
+
+    public static partial class Util
+    {
+
+        /// <summary>
+        /// create a dynamic object containing given set of properties
+        /// </summary>        
+        public static dynamic MakeDynamic(params (string name, object value)[] items)
+        {
+            var fieldeo = new ExpandoObject() as IDictionary<string, object>;
+
+            foreach (var x in items) fieldeo.Add(x.name, x.value);            
+
+            return fieldeo as dynamic;
+        }
+
+    }
+
+}
