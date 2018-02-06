63dd24e5cb32f84352df897c4ae854b84596f3e0
Lorenzo Delana <oss.devel@searchathing.com>
2018-02-06 14:38:27 +0100

    CopyFrom util

--- Dynamic.cs
+++ Dynamic.cs
@@ -1,7 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Dynamic;
-
 namespace SearchAThing.NETCoreUtil
 {
 
---
+++ Reflection.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace SearchAThing.NETCoreUtil
+{
+
+    public static partial class Util
+    {
+
+        /// <summary>
+        /// copy properties from other object excluding given list of property names
+        /// </summary>        
+        internal static T CopyFrom<T>(this T obj, T other, bool onlyPrimitives = true, params string[] exclude)
+        {
+            var type = typeof(T);
+
+            foreach (var p in type.GetProperties().Where(x => (onlyPrimitives ? x.PropertyType.IsPrimitive : true) && !exclude.Contains(x.Name)))
+                p.SetValue(obj, p.GetValue(other));
+
+            return obj;
+        }
+
+        /// <summary>
+        /// copy primitive properties from other object excluding given list of property names
+        /// </summary>        
+        internal static T CopyFrom<T>(this T obj, T other, params string[] exclude)
+        {
+            return obj.CopyFrom(other, onlyPrimitives: true, exclude: exclude);
+        }
+
+    }
+
+}
