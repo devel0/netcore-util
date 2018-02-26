9c78fc8b9fd46c89c16eee72350190557a5fe072
Lorenzo Delana <oss.devel@searchathing.com>
2018-02-26 14:35:03 +0100

    various number and string extensions

---
+++ Number.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using static System.Math;
+
+namespace SearchAThing.NETCoreUtil
+{
+
+    public static partial class Util
+    {
+
+        /// <summary>
+        /// Returns true if two numbers are equals using a default tolerance of 1e-6 about the smaller one.
+        /// </summary>        
+        public static bool EqualsAutoTol(this double x, double y, double precision = 1e-6)
+        {
+            return Abs(x - y) < Min(x, y) * precision;
+        }
+
+
+        /// <summary>
+        /// Round the given value using the multiple basis
+        /// </summary>        
+        public static double MRound(this double value, double multiple)
+        {
+            if (Abs(multiple) < double.Epsilon) return value;
+
+            var p = Round(value / multiple);
+
+            return Truncate(p) * multiple;
+        }
+
+
+        /// <summary>
+        /// Round the given value using the multiple basis
+        /// if null return null
+        /// </summary>        
+        public static double? MRound(this double? value, double multiple)
+        {
+            if (value.HasValue)
+                return value.Value.MRound(multiple);
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Round the given value using the multiple basis
+        /// </summary>        
+        public static double MRound(this double value, double? multiple)
+        {
+            if (multiple.HasValue)
+                return value.MRound(multiple.Value);
+            else
+                return value;
+        }
+
+        /// <summary>
+        /// convert given angle(rad) to degree
+        /// </summary>        
+        public static double ToDeg(this double angleRad)
+        {
+            return angleRad / PI * 180.0;
+        }
+
+        /// <summary>
+        /// convert given angle(grad) to radians
+        /// </summary>        
+        public static double ToRad(this double angleGrad)
+        {
+            return angleGrad / 180.0 * PI;
+        }
+
+        /// <summary>
+        /// Return an invariant string representation rounded to given dec.        
+        public static string Stringify(this double x, int dec)
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{0}", Round(x, dec));
+        }
+
+        /// <summary>
+        /// Magnitude of given number. (eg. 190 -> 1.9e2 -> 2)
+        /// (eg. 0.0034 -> 3.4e-3 -> -3)
+        /// </summary>        
+        public static int Magnitude(this double value)
+        {
+            var a = Abs(value);
+
+            if (a < double.Epsilon) return 0;
+
+            return (int)Floor(Log10(a));
+        }
+
+        /// <summary>
+        /// Invariant culture double parse
+        /// </summary>        
+        public static double InvDoubleParse(this string str)
+        {
+            return double.Parse(str, CultureInfo.InvariantCulture);
+        }
+
+        public static double Mean(this IEnumerable<double> set)
+        {
+            var v = 0.0;
+            int cnt = 0;
+            foreach (var x in set) { v += x; ++cnt; }
+            return v / cnt;
+        }
+
+        public static string ToString(this double d, int significantDigits)
+        {
+            var decfmt = "#".Repeat(significantDigits);
+            return string.Format(CultureInfo.InvariantCulture, "{0:0." + decfmt + "}", d);
+        }
+
+        public static bool EqualsTol(this double x, double tol, double y)
+        {
+            return Abs(x - y) <= tol;
+        }
+
+        public static bool EqualsAutoTol(this double x, double y)
+        {
+            return x.EqualsTol(Abs(x * 1e-6), y);
+        }
+
+        public static bool GreatThanTol(this double x, double tol, double y)
+        {
+            return x > y && !x.EqualsTol(tol, y);
+        }
+
+        public static bool GreatThanOrEqualsTol(this double x, double tol, double y)
+        {
+            return x > y || x.EqualsTol(tol, y);
+        }
+
+        public static bool LessThanTol(this double x, double tol, double y)
+        {
+            return x < y && !x.EqualsTol(tol, y);
+        }
+
+        public static bool LessThanOrEqualsTol(this double x, double tol, double y)
+        {
+            return x < y || x.EqualsTol(tol, y);
+        }
+
+        public static int CompareTol(this double x, double tol, double y)
+        {
+            if (x.EqualsTol(tol, y)) return 0;
+            if (x < y) return -1;
+            return 1; // x > y
+        }
+
+        /// <summary>
+        /// eval if a number fits in given range
+        /// eg.
+        /// - "[0, 10)" are numbers from 0 (included) to 10 (excluded)
+        /// - "[10, 20]" are numbers from 10 (included) to 20 (included)
+        /// - "(30,)" are numbers from 30 (excluded) to +infinity
+        /// </summary>        
+        public static bool IsInRange(this double nr, double tol, string range)
+        {
+            var s = range.Trim();
+            var fromIncluded = s.StartsWith("[");
+            var toIncluded = s.EndsWith("]");
+            var ss = s.TrimStart('[', '(').TrimEnd(']').TrimEnd(')').Split(',');
+            var from = ss[0].Trim().Length == 0 ? new double?() : double.Parse(ss[0], CultureInfo.InvariantCulture);
+            var to = ss[1].Trim().Length == 0 ? new double?() : double.Parse(ss[1], CultureInfo.InvariantCulture);
+
+            if (!from.HasValue && !to.HasValue) return true;
+
+            var contains = true;
+
+            if (from.HasValue)
+            {
+                if (fromIncluded)
+                    contains = contains && nr.GreatThanOrEqualsTol(tol, from.Value);
+                else
+                    contains = contains && nr.GreatThanTol(tol, from.Value);
+            }
+
+            if (to.HasValue)
+            {
+                if (toIncluded)
+                    contains = contains && nr.LessThanOrEqualsTol(tol, to.Value);
+                else
+                    contains = contains && nr.LessThanTol(tol, to.Value);
+            }
+
+            return contains;
+        }
+
+        /// <summary>
+        /// returns 1.0 if n>=0
+        /// -1 otherwise
+        /// </summary>        
+        public static double Sign(this int n)
+        {
+            if (n >= 0) return 1.0;
+            return -1.0;
+        }
+
+        /// <summary>
+        /// returns 1.0 if n>=0
+        /// -1 otherwise
+        /// </summary>        
+        public static double Sign(this double n)
+        {
+            if (n >= 0) return 1.0;
+            return -1.0;
+        }
+
+    }
+
+}
---
+++ String.cs
@@ -0,0 +1,385 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.FormattableString;
+
+namespace SearchAThing.NETCoreUtil
+{
+
+    public static partial class Util
+    {
+
+        /// <summary>
+        /// Returns the given string stripped from the given part if exists at beginning.
+        /// </summary>        
+        public static string StripBegin(this string str, char c, bool ignoreCase = false)
+        {
+            if (str.Length > 0 && (ignoreCase ? (char.ToUpper(str[0]) == char.ToUpper(c)) : (str[0] == c)))
+                return str.Substring(1, str.Length - 1);
+            else
+                return str;
+        }
+
+        /// <summary>
+        /// Returns the given string stripped from the given part if exists at beginning.
+        /// </summary>        
+        public static string StripBegin(this string str, string partToStrip, bool ignoreCase = false)
+        {
+            if (str.StartsWith(partToStrip, ignoreCase, CultureInfo.CurrentCulture))
+            {
+                var ptsl = partToStrip.Length;
+                return str.Substring(ptsl, str.Length - ptsl);
+            }
+            else
+                return str;
+        }
+
+        /// <summary>
+        /// Returns the given string stripped from the given part if exists at end.
+        /// </summary>        
+        public static string StripEnd(this string str, char c, bool ignoreCase = false)
+        {
+            if (str.Length > 0 && (ignoreCase ? (char.ToUpper(str[str.Length - 1]) == char.ToUpper(c)) : (str[str.Length - 1] == c)))
+                return str.Substring(0, str.Length - 1);
+            else
+                return str;
+        }
+
+        /// <summary>
+        /// Returns the given string stripped from the given part if exists at end.
+        /// </summary>        
+        public static string StripEnd(this string str, string partToStrip, bool ignoreCase = false)
+        {
+            if (str.EndsWith(partToStrip, ignoreCase, CultureInfo.CurrentCulture))
+                return str.Substring(0, str.Length - partToStrip.Length);
+            else
+                return str;
+        }
+
+        /// <summary>
+        /// extract line from string buffer
+        /// </summary>        
+        public static string NextLine(this string txt, ref int off)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Smart line splitter that split a text into lines whatever unix or windows line ending style.
+        /// By default its remove empty lines.
+        /// </summary>        
+        /// <param name="removeEmptyLines">If true remove empty lines.</param>        
+        public static IEnumerable<string> Lines(this string txt, bool removeEmptyLines = true)
+        {
+            var q = txt.Replace("\r\n", "\n").Split('\n');
+
+            if (removeEmptyLines)
+                return q.Where(r => r.Trim().Length > 0);
+            else
+                return q;
+        }
+
+        /// <summary>
+        /// Returns a human readable bytes length. (eg. 1000, 1K, 1M, 1G, 1T)
+        /// if onlyBytesUnit is set to false it will enable representation through K, M, G, T suffixes
+        /// </summary>        
+        public static string HumanReadable(this long bytes, bool onlyBytesUnit = true, long bytesMultiple = 1L, int decimals = 1)
+        {
+            var k = 1024L;
+            var m = k * 1024;
+            var g = m * 1024;
+            var t = g * 1024;
+
+            if (bytesMultiple != 1L) bytes = (long)((double)bytes).MRound(bytesMultiple);
+
+            if (bytes < k) { if (onlyBytesUnit) return $"{bytes}"; else return Invariant($"{bytes} b"); }
+            else if (bytes >= k && bytes < m) return string.Format("{0,6:0." + "0".Repeat(decimals) + "} Kb", ((double)bytes) / k);
+            else if (bytes >= m && bytes < g) return string.Format("{0,6:0." + "0".Repeat(decimals) + "} Mb", ((double)bytes) / m);
+            else if (bytes >= g && bytes < t) return string.Format("{0,6:0." + "0".Repeat(decimals) + "} Gb", ((double)bytes) / g);
+            else return string.Format("{0:0." + "0".Repeat(decimals) + "} Tb", ((double)bytes) / t);
+        }
+
+        /// <summary>
+        /// Repeat given string for cnt by concatenate itself
+        /// </summary>        
+        public static string Repeat(this string s, int cnt)
+        {
+            var sb = new StringBuilder();
+
+            while (cnt > 0)
+            {
+                sb.Append(s);
+                --cnt;
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// convert wildcard pattern to regex
+        /// the asterisk '*' character replace any group of chars
+        /// the question '?' character replace any single character
+        /// </summary>        
+        public static string WildcardToRegex(this string pattern)
+        {
+            return $"^{Regex.Escape(pattern).Replace("\\*", ".*").Replace('?', '.')}$";
+        }
+
+        /// <summary>
+        /// return true if given string matches the given pattern
+        /// the asterisk '*' character replace any group of chars
+        /// the question '?' character replace any single character
+        /// </summary>                
+        public static bool WildcardMatch(this string str, string pattern, bool caseSentitive = true)
+        {
+            var regexStr = pattern.WildcardToRegex();
+
+            Regex regex = null;
+            if (caseSentitive)
+                regex = new Regex(regexStr);
+            else
+                regex = new Regex(regexStr, RegexOptions.IgnoreCase);
+
+            return regex.IsMatch(str);
+        }
+
+        /// <summary>
+        /// split string with given separator string
+        /// </summary>        
+        public static string[] Split(this string str, string sepStr)
+        {
+            return str.Split(sepStr.ToArray(), StringSplitOptions.None);
+        }
+
+        /// <summary>
+        /// return dynamic array from given [[xx],[yy],...] json array
+        /// </summary>        
+        public static dynamic GetJsonArray(this string jsonDumps)
+        {
+            return ((dynamic)JObject.Parse($"{{a:{jsonDumps}}}")).a;
+        }
+
+        /// <summary>
+        /// parse given array of doubles ( invariant ) comma separated
+        /// </summary>        
+        public static double[] Import(this string ary)
+        {
+            if (string.IsNullOrEmpty(ary.Trim())) return new double[] { };
+
+            return ary.Split(',').Select(w => double.Parse(w, CultureInfo.InvariantCulture)).ToArray();
+        }
+
+        /// <summary>
+        /// export to a string ( invariant ) comma separated
+        /// </summary>        
+        public static string Export(this double[] ary)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var x in ary)
+            {
+                if (sb.Length > 0) sb.Append(",");
+                sb.Append(string.Format(CultureInfo.InvariantCulture, "{0}", x));
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// convert a string that exceed N given characters length to {prefix}{latest N chars}
+        /// </summary>        
+        public static string Latest(this string str, int last_n_chars, string prefix_if_exceed = "...")
+        {
+            if (str.Length <= last_n_chars)
+                return str;
+            else
+                return $"{prefix_if_exceed}{str.Substring(str.Length - last_n_chars, last_n_chars)}";
+        }
+
+        /// <summary>
+        /// check if given string contains the part ( ignoring case )
+        /// </summary>        
+        public static bool ContainsIgnoreCase(this string str, string part)
+        {
+            return str.ToUpper().Contains(part.ToUpper());
+        }
+
+        /// <summary>
+        /// convert invalid worksheet characters :\/?*[]' into underscore
+        /// </summary>        
+        public static string NormalizeWorksheetName(this string s)
+        {
+            return Regex.Replace(s, $"[{Regex.Escape(@":\/?*[]'").Replace("]", "\\]")}]", "_");
+        }
+
+        public static string NormalizeFilename(this string filename, char subst = '_')
+        {
+            string res = "";
+            var invalid_chars = System.IO.Path.GetInvalidFileNameChars();
+            for (int i = 0; i < filename.Length; ++i)
+            {
+                if (invalid_chars.Any(t => t == filename[i]))
+                    res += subst;
+                else
+                    res += filename[i];
+            }
+
+            return res;
+        }
+
+        public static StringWrapper ToStringWrapper(this StringBuilder sb)
+        {
+            return new StringWrapper() { str = sb.ToString() };
+        }
+
+        public static string ToJson(this object o)
+        {
+            return JsonConvert.SerializeObject(o);
+        }
+
+        /// <summary>
+        /// removes all characters that aren't 0-9 dot or comma
+        /// </summary>        
+        public static string TrimNonNumericCharacters(this string s)
+        {
+            string res = "";
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (char.IsNumber(s[i]) || s[i] == '.' || s[i] == ',') res += $"{s[i]}";
+            }
+
+            return res;
+        }
+
+        public static int ParseInt(this string s) { return int.Parse(s); }
+
+        /// <summary>
+        /// return yyyy-MM-dd HH:mm.ss representation
+        /// </summary>        
+        public static string InvarianteDateTime(this DateTime dt, string datesep = "-", string timesep = ":")
+        {
+            return $"{dt.InvariantDate(datesep)} {dt.InvariantTime(timesep)}";
+        }
+
+        /// <summary>
+        /// return yyyy-MM-dd representation
+        /// </summary>        
+        public static string InvariantDate(this DateTime dt, string sep = "-")
+        {
+            return string.Format("{0:0000}{1}{2:00}{3}{4:00}", dt.Year, sep, dt.Month, sep, dt.Day);
+        }
+
+        /// <summary>
+        /// return HH:mm.ss representation
+        /// </summary>        
+        public static string InvariantTime(this DateTime dt, string sep = "-")
+        {
+            return string.Format("{0:00}{1}{2:00}{3}{4:00}", dt.Hour, sep, dt.Minute, sep, dt.Second);
+        }
+
+        /// <summary>
+        /// retrieve nr. of occurrence of given pattern through regex
+        /// </summary>        
+        public static int RegexMatch(this string s, string pattern, RegexOptions opt = RegexOptions.None)
+        {
+            return Regex.Matches(s, pattern, opt).Count;
+        }
+
+    }
+
+    public class StringWrapperLineReader
+    {
+
+        public StringWrapper Strw { get; private set; }
+
+        IEnumerator<string> en = null;
+
+        bool has_next = false;
+
+        public StringWrapperLineReader(StringWrapper strw)
+        {
+            Strw = strw;
+
+            en = strw.Lines().GetEnumerator();
+
+            has_next = en.MoveNext();
+        }
+
+        public bool HasNext() { return has_next; }
+
+        public StringWrapper GetNext()
+        {
+            var res = new StringWrapper() { str = en.Current };
+            has_next = en.MoveNext();
+            return res;
+        }
+
+    }
+
+    /// <summary>
+    /// wrapper for memory optimized string argument passing
+    /// </summary>
+    public class StringWrapper
+    {
+        public string str;
+
+        public StringWrapperLineReader LineReader()
+        {
+            return new StringWrapperLineReader(this);
+        }
+
+        public IEnumerable<string> Lines()
+        {
+            int back_off = 0;
+            int off = 0;
+            int maxlen = str.Length;
+
+            while (off < maxlen)
+            {
+                while (off < maxlen && str[off] != '\r' && str[off] != '\n') ++off;
+                if (off == maxlen)
+                {
+                    if (back_off != off)
+                    {
+                        var trimend_cnt = 0;
+                        {
+                            var j = off - 1;
+                            while (j >= 0 && str[j] == '\r' || str[j] == '\n') { --j; ++trimend_cnt; }
+                        }
+
+                        yield return str.Substring(back_off, off - back_off + ((off == maxlen) ? 0 : 1));
+                    }
+                }
+                ++off;
+
+                if (off > 0 && str[off - 1] == '\r' && str[off] == '\n') ++off;
+
+                if (back_off != off)
+                {
+                    var trimend_cnt = 0;
+                    {
+                        var j = off - 1;
+                        while (j >= 0 && str[j] == '\r' || str[j] == '\n') { --j; ++trimend_cnt; }
+                    }
+                    yield return str.Substring(back_off, off - back_off - trimend_cnt);
+                }
+
+                back_off = off;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return str;
+        }
+
+    }
+
+}
