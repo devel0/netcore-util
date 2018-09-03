2f919a033ee3bd2f7f945002c40edc44638d8a24
Lorenzo Delana <oss.devel@searchathing.com>
2018-09-03 20:24:25 +0200

    adding unit tests

---
+++ test/UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace tmp1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
