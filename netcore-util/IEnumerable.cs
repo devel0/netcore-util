4eeffd2287f12f70387167eacca3834afc157d67
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-07-24 00:49:24 +0200

    WithIndexIsLast extension

--- netcore-util/IEnumerable.cs
+++ netcore-util/IEnumerable.cs
@@ -14,7 +14,27 @@ namespace SearchAThing
         /// enumerable extension to enumerate itself into an (item, idx) set
         /// </summary>
         public static IEnumerable<(T item, int idx)> WithIndex<T>(this IEnumerable<T> en)
-            => en.Select((item, idx) => (item, idx)); 
+            => en.Select((item, idx) => (item, idx));
+
+        /// <summary>
+        /// enumerable extension to enumerate itself into an (item, idx, isLast) set
+        /// </summary>
+        public static IEnumerable<(T item, int idx, bool isLast)> WithIndexIsLast<T>(this IEnumerable<T> en)
+        {
+            var enm = en.GetEnumerator();
+
+            var idx = 0;
+            var isLast = !enm.MoveNext();
+            var item = default(T);
+            while (!isLast)
+            {
+                item = enm.Current;
+                isLast = !enm.MoveNext();
+                yield return (item, idx, isLast);
+                if (isLast) yield break;
+                ++idx;                
+            }
+        }
 
     }
 
