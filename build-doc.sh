e80a387718ea87b5066c37fa7ef0829972fd3ccf
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-08-09 01:48:38 +0200

    netcore 3

--- build-doc.sh
+++ build-doc.sh
@@ -3,7 +3,7 @@
 exdir=$(dirname `readlink -f "$0"`)
 
 dotnet build /p:CopyLocalLockFileAssemblies=true
-docpal --proptable --mgtable --mgspace -out "$exdir"/tmp-api-doc "$exdir"/netcore-util/bin/Debug/netstandard2.0/netcore-util.dll
+docpal --proptable --mgtable --mgspace -out "$exdir"/tmp-api-doc "$exdir"/netcore-util/bin/Debug/netstandard2.1/netcore-util.dll
 
 rm -fr "$exdir"/doc/api
 mv "$exdir"/tmp-api-doc/docs/SearchAThing "$exdir"/doc/api
