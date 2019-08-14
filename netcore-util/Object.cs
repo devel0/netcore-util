99fa634aad03f12c39d00af6c01c933f710a4f9a
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-08-15 01:03:35 +0200

    object Eval helper

---
+++ netcore-util/Object.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.FormattableString;
+using static System.Math;
+
+namespace SearchAThing
+{
+
+    public static partial class UtilExt
+    {
+
+        /// <summary>
+        /// useful to decide action checking for nullity when object retrieval
+        /// can consume cpu;
+        /// eg. myobj.Eval((x) => (x == null) ? "" : x)
+        /// </summary>
+        public static U Eval<T, U>(this T obj, Func<T, U> fn)
+        {
+            return fn(obj);
+        }
+
+    }
+
+}
