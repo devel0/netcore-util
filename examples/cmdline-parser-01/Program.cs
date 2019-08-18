b33199cda2c4044c533a598044a32d851fead312
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-08-18 02:57:54 +0200

    cmdline parser multi level

---
+++ examples/cmdline-parser-01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using SearchAThing;
+
+namespace cmdline_parser_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmdline = new CmdlineParser("sample program", (parser) =>
+            {
+                var flag_i = parser.AddShort("i", "print filename stats");                
+                var file1 = parser.AddParameter("file1", "1th pathfilename to test", mandatory: false);
+                var file2 = parser.AddParameter("file2", "2th pathfilename to test", mandatory: false);
+
+                parser.OnCmdlineMatch = () =>
+                {
+                    System.Console.WriteLine($"flag_i:{(bool)flag_i}");
+                    System.Console.WriteLine($"file1:{(string)file1}");
+                    System.Console.WriteLine($"file2:{(string)file2}");
+
+                    if (flag_i)
+                    {
+                        if (file1) System.Console.WriteLine($"Filesize [{(string)file1}] = {new FileInfo((string)file1).Length}");
+                        if (file2) System.Console.WriteLine($"Filesize [{(string)file2}] = {new FileInfo((string)file2).Length}");
+                    }
+                };
+            });
+
+            cmdline.Run(args);
+        }
+    }
+}
---
+++ examples/cmdline-parser-04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using SearchAThing;
+
+namespace cmdline_parser_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmdline = new CmdlineParser("sample program", (parser) =>
+            {
+                parser.AddShort("b", "base option");
+                parser.AddCommand("cmd1", "command 1 with no sub commands");
+                parser.AddCommand("cmd2", "command 2 with sub commands", new CmdlineParser("invoke cmd2 sub commands", (parser2) =>                
+                {
+                    parser2.AddShort("o2", "option on cmd2");
+                    parser2.AddCommand("cmd-a", "command a");
+                    parser2.AddCommand("cmd-b", "command b");
+
+                    parser2.OnCmdlineMatch = () =>
+                    {
+                        System.Console.WriteLine(parser2);
+                    };
+                }));
+
+                parser.OnCmdlineMatch = () =>
+                {
+                    System.Console.WriteLine(parser);
+                };
+            });
+
+            cmdline.Run(args);
+        }
+    }
+}
