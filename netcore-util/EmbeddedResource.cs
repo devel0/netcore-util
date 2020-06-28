1ebdce810060f85e8cb2987520e6427c2223af2f
Lorenzo Delana <lorenzo.delana@gmail.com>
2020-06-28 09:01:13 +0200

    retrieve embedded resource as ext method that start from resource name string

--- netcore-util/EmbeddedResource.cs
+++ netcore-util/EmbeddedResource.cs
@@ -5,67 +5,106 @@ using System.IO;
 using System.IO.Compression;
 using System.Reflection;
 
-namespace SearchAThing.Util
+namespace SearchAThing
 {
 
-    public static partial class Toolkit
+    public static partial class UtilExt
     {
 
         /// <summary>
-        /// deflate embedded resource
+        /// retrieve embedded resource file content and read into a string
         /// </summary>
-        public static Stream DeflateEmbeddedResource(Assembly assembly, string resource)
+        /// <param name="resourceName">name of resource (eg. namespace.filename.ext)</param>
+        /// <typeparam name="T">Type for which lookup assembly (eg. namespace.classname)</typeparam>
+        public static string GetEmbeddedFileContent<T>(this string resourceName) where T : class
         {
-            using (var s = assembly.GetManifestResourceStream(resource))
+            var assembly = typeof(T).GetTypeInfo().Assembly;
+            string res = "";
+            using (var resource = assembly.GetManifestResourceStream(resourceName))
             {
-                var ms = new MemoryStream();
-                using (var ds = new DeflateStream(s, CompressionMode.Decompress, true))
+                using (var sr = new StreamReader(resource))
                 {
-                    var buf = new byte[4096];
-                    int read;
-                    while ((read = ds.Read(buf, 0, buf.Length)) != 0)
-                        ms.Write(buf, 0, read);
+                    res = sr.ReadToEnd();
                 }
-                ms.Seek(0, SeekOrigin.Begin);
-                return ms;
             }
+            return res;
         }
 
-        /// <summary>
-        /// save given embedded resource to file
-        /// </summary>
-        public static void SaveEmbeddedResourceToFile(string resource, string dstPathfilename, bool deflate = false)
+    }
+
+    namespace Util
+    {
+
+        public static partial class Toolkit
         {
-            var assembly = Assembly.GetCallingAssembly();
-            if (deflate)
-            {
-                using (var fs = DeflateEmbeddedResource(assembly, resource))
-                {
-                    using (var dstfs = new FileStream(dstPathfilename, FileMode.Create))
-                    {
-                        fs.CopyTo(dstfs);
-                    }
-                }
-            }
-            else
+
+            /// <summary>
+            /// deflate embedded resource
+            /// </summary>
+            public static Stream DeflateEmbeddedResource(Assembly assembly, string resource)
             {
                 using (var s = assembly.GetManifestResourceStream(resource))
                 {
-                    using (var dstfs = new FileStream(dstPathfilename, FileMode.Create))
+                    var ms = new MemoryStream();
+                    using (var ds = new DeflateStream(s, CompressionMode.Decompress, true))
                     {
-                        s.CopyTo(dstfs);
+                        var buf = new byte[4096];
+                        int read;
+                        while ((read = ds.Read(buf, 0, buf.Length)) != 0)
+                            ms.Write(buf, 0, read);
+                    }
+                    ms.Seek(0, SeekOrigin.Begin);
+                    return ms;
+                }
+            }
+
+            /// <summary>
+            /// save given embedded resource to file
+            /// </summary>
+            public static void SaveEmbeddedResourceToFile(string resource, string dstPathfilename, bool deflate = false)
+            {
+                var assembly = Assembly.GetCallingAssembly();
+                if (deflate)
+                {
+                    using (var fs = DeflateEmbeddedResource(assembly, resource))
+                    {
+                        using (var dstfs = new FileStream(dstPathfilename, FileMode.Create))
+                        {
+                            fs.CopyTo(dstfs);
+                        }
+                    }
+                }
+                else
+                {
+                    using (var s = assembly.GetManifestResourceStream(resource))
+                    {
+                        using (var dstfs = new FileStream(dstPathfilename, FileMode.Create))
+                        {
+                            s.CopyTo(dstfs);
+                        }
                     }
                 }
             }
-        }
 
-        /// <summary>
-        /// retrieve current executing assembly resource names
-        /// </summary>
-        public static IEnumerable<string> GetEmbeddedResourceNames()
-        {
-            var assembly = Assembly.GetCallingAssembly();
-            return assembly.GetManifestResourceNames();
+            /// <summary>
+            /// retrieve current executing assembly resource names
+            /// </summary>
+            public static IEnumerable<string> GetEmbeddedResourceNames()
+            {
+                var assembly = Assembly.GetCallingAssembly();
+                return assembly.GetManifestResourceNames();
+            }
+
+            /// <summary>
+            /// retrieve the list of embedded resource names from given Type
+            /// </summary>
+            /// <typeparam name="T">Type for which lookup assembly (eg. namespace.classname)</typeparam>
+            public static string[] GetEmbeddedResourcesList<T>() where T : class
+            {
+                var assembly = typeof(T).GetTypeInfo().Assembly;
+                return assembly.GetManifestResourceNames();
+            }
+
         }
 
     }
