da96e55c76228e33b1e38d933e21070981551727
Lorenzo Delana <lorenzo.delana@gmail.com>
2020-07-10 23:25:46 +0200

    enum WithPrev, WithNext extenions

--- netcore-util/IEnumerable.cs
+++ netcore-util/IEnumerable.cs
@@ -42,6 +42,126 @@ namespace SearchAThing
             }
         }
 
+        /// <summary>
+        /// enumerate given items returning a tuple with nullable ( for first hit ) prev element
+        /// </summary>
+        public static IEnumerable<(T? prev, T item)> WithPrevPrimitive<T>(this IEnumerable<T> en) where T : struct
+        {
+            var enm = en.GetEnumerator();
+
+            T? prev = null;
+            var item = default(T);
+            while (enm.MoveNext())
+            {
+                item = enm.Current;
+                yield return (prev, item);
+                prev = item;
+            }
+        }
+
+        /// <summary>
+        /// enumerate given items returning a tuple with null ( for first hit ) prev element
+        /// </summary>
+        public static IEnumerable<(T prev, T item)> WithPrev<T>(this IEnumerable<T> en) where T : class
+        {
+            var enm = en.GetEnumerator();
+
+            T prev = null;
+            var item = default(T);
+            while (enm.MoveNext())
+            {
+                item = enm.Current;
+                yield return (prev, item);
+                prev = item;
+            }
+        }
+
+        /*
+        // compiler err:
+        // A nullable type parameter must be known to be a value type or non-nullable reference type. Consider adding a 'class', 'struct', or type constraint.        
+
+        /// <summary>
+        /// enumerate given items returning a tuple with null ( for first hit ) prev element
+        /// </summary>
+        public static IEnumerable<(T? prev, T item)> WithPrevX<T>(this IEnumerable<T> en)
+        {
+            var enm = en.GetEnumerator();
+
+            T prev = null;
+            var item = default(T);
+            while (enm.MoveNext())
+            {
+                item = enm.Current;
+                yield return (prev, item);
+                prev = item;
+            }
+        }
+        */
+
+        /// <summary>
+        /// enumerate given items returning a tuple with null ( for last hit ) next element
+        /// </summary>
+        public static IEnumerable<(T item, T? next)> WithNextPrimitive<T>(this IEnumerable<T> en) where T : struct
+        {
+            var enm = en.GetEnumerator();
+
+            T? prev = null;
+            var item = default(T);
+            var isLast = !enm.MoveNext();
+            while (!isLast)
+            {
+                item = enm.Current;
+                isLast = !enm.MoveNext();
+                if (isLast)
+                {
+                    yield return (item, null);
+                    yield break;
+                }
+                else
+                {
+                    if (prev == null)
+                        prev = item;
+                    else
+                    {
+                        yield return (prev.Value, item);
+                        prev = item;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// enumerate given items returning a tuple with null ( for last hit ) next element
+        /// </summary>
+        public static IEnumerable<(T item, T next)> WithNext<T>(this IEnumerable<T> en) where T : class
+        {
+            var enm = en.GetEnumerator();
+
+            T prev = null;
+            var item = default(T);
+            var isLast = !enm.MoveNext();
+            while (!isLast)
+            {
+                item = enm.Current;
+                isLast = !enm.MoveNext();
+                if (isLast)
+                {
+                    yield return (item, null);
+                    yield break;
+                }
+                else
+                {
+                    if (prev == null)
+                        prev = item;
+                    else
+                    {
+                        yield return (prev, item);
+                        prev = item;
+                    }
+                }
+            }
+        }
+
     }
 
 }
