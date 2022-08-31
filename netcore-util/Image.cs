8214ad5db930c0aafa7c248341962ce511250fa5
Lorenzo Delana <lorenzo.delana@gmail.com>
2022-08-31 23:49:21 +0200

    v4.1.0 ; GetImageNfo helper

---
+++ netcore-util/Image.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using ImageMagick;
+
+namespace SearchAThing
+{
+
+
+    public static partial class UtilToolkit
+    {
+        
+        /// <summary>
+        /// retrieve image nfo
+        /// </summary>
+        public static MagickImageInfo GetImageNfo(string pathfilename) => new MagickImageInfo(pathfilename);
+        
+    }
+
+}
