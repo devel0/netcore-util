5c0497d6d04022ccd70c6d51d0fbecb3d76de768
Lorenzo Delana <lorenzo.delana@gmail.com>
2022-09-21 10:13:50 +0200

    readonly hashset

---
+++ netcore-util/ReadOnlyHashSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SearchAThing
+{
+
+    public interface IReadOnlyHashSet<T> : IReadOnlyCollection<T>
+    {
+        public bool Contains(T x);
+    }
+
+    /// <summary>
+    /// enclose hashset to a readonly collection
+    /// </summary>
+    public class ReadOnlyHashSet<T> : IReadOnlyHashSet<T>
+    {
+        HashSet<T> hs;
+
+        public ReadOnlyHashSet(HashSet<T> hs)
+        {
+            this.hs = hs;
+        }
+
+        public int Count => hs.Count;
+
+        public bool Contains(T x) => hs.Contains(x);
+
+        public IEnumerator<T> GetEnumerator() => hs.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => hs.GetEnumerator();
+
+    }
+
+
+}
