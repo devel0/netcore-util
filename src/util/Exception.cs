f7c64e2f61a7182a85939adbd177fea6c3c393f6
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-04-16 01:42:51 +0200

    internal error exception type

--- src/util/Exception.cs
+++ src/util/Exception.cs
@@ -70,20 +70,3 @@ public static partial class Ext
     }
 
 }
-
-/// <summary>
-/// InternalError exception
-/// </summary>
-public class InternalError : Exception
-{
-
-    /// <summary>
-    /// Internal exception constructor, some assert failed
-    /// </summary>
-    /// <param name="msg">assert fail msg</param>
-    public InternalError(string msg) : base(msg)
-    {
-
-    }
-
-}
